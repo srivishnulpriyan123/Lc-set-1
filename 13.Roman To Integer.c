dba8bef811fa47cfade43f4f5f17c88053293695
SRI VISHNU PRIYAN V <srivishnupriyan11@gmail.com>
2024-11-25 08:56:40 +0530

    Update 13.Roman To Integer.c

--- 13.Roman To Integer.c
+++ 13.Roman To Integer.c
@@ -1,78 +1,46 @@
 int romanToInt(char* s)
 {
-    int tot=0,current,next;
-    int len=strlen(s);
-    for(int i=0;i<len;i++)
-    {
-        switch(s[i])
-        {
-            case 'I':
-              current=1;
-              break;
-            case 'V':
-               current=5;
-               break;
-            case 'X':
-               current=10;
-               break;
-            case 'L':
-               current=50;
-               break;
-            case 'C':
-               current=100;
-               break;
-            case 'D':
-               current=500;
-               break;
-            case 'M':
-               current=1000;
-               break;
-            default:
-                current=0;
-                break;                   
-        }
-        if(i+1<len)
-        {
-            switch(s[i+1])
-            {
-            case 'I':
-              next=1;
-              break;
-            case 'V':
-               next=5;
-               break;
-            case 'X':
-               next=10;
-               break;
-            case 'L':
-               next=50;
-               break;
-            case 'C':
-               next=100;
-               break;
-            case 'D':
-               next=500;
-               break;
-            case 'M':
-               next=1000;
-               break;
-            default:
-                next=0;
-                break;  
-            }
-            if(current < next)
-            {
-                tot -= current;
-            }
-            else
-            {
-                tot += current;
-            }
-        }
-        else
-        {
-            tot += current;
-        }
+    int i=0,a[strlen(s)];
+	while(s[i]!='\0')
+	{
+		switch(s[i])
+		{
+			case 'I':
+			a[i]=1;
+			break;
+		    case 'V':
+			a[i]=5;
+			break;
+			case 'X':
+			a[i]=10;
+			break;
+			case 'L':
+			a[i]=50;
+			break;
+			case 'C':
+			a[i]=100;
+			break;
+			case 'D':
+			a[i]=500;
+			break;
+			case 'M':
+			a[i]=1000;
+			break;
+		}
+	    i++;
     }
-   return tot;
+	int k=0;
+	for(int i=0;i<strlen(s);i++)
+	{
+	if(a[i]>=a[i+1])
+	{
+		k=a[i]+k;
+	}
+	else
+	{
+		k=k+a[i+1]-a[i];
+		i++;
+	}
+	}
+	return k;
 }
